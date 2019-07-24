71af9a4e2e1827c08483885a67bf5f65d121af84
Emanuelle <emanuelle.sanntoslima@gmail.com>
2019-07-24 18:01:04 -0300

    Realizar atividade sobre responsividade

---
+++ script.js
@@ -0,0 +1,12 @@
+let hamburguer = document.querySelector("#mobile");
+let menu = document.querySelector("section");
+let tracos = document.querySelectorAll("span");
+
+
+function controlarMenu(){
+    menu.classList.toggle("aparecerMenu");
+    tracos[1].classList.toggle("sumirIgualTiro");
+    tracos[0].classList.toggle("virarX1");
+    tracos[2].classList.toggle("virarX2");
+}
+mobile.onclick = controlarMenu;
